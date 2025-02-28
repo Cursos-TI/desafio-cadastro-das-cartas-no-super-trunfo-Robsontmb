740f290d3d645596f4b2d39427de0d2d10bcd30b
Robsontmb <rtadeumb@hotmail.com>
2025-02-28 23:36:28 +0000

    trabalho 2 super trunfo

--- trabalho2supertrunfo.c
+++ trabalho2supertrunfo.c
@@ -2,9 +2,72 @@
 
 int main () {
 
+    int populacao, populacao2;
+    float area, area2;
+    long long int pib, pib2;
+    int pontosturisticos, pontosturisticos2;
+    double densidadepopulacional, densidadepopulacional2;
+    double pibpercapta, pibpercapta2;
+    double superpoder, superpoder2;
+    int opcao;
+
+    printf("Bem vindo ao jogo Supertrunfo países!\n");
+    printf("Escolha uma opção:\n");
+    printf("1. Iniciar jogo.\n");
+    printf("2. como jogar.\n");
+    printf("3. Créditos.\n");
+    printf("4. Sair.\n");
+
+    switch(opcao){
+
+        case 1:
+
+
+        break;
+
+        case 2:
+
+
+        break;
+
+        case 3:
+        printf("O jogo é bem simples, o jogador deve escolher uma carta referente a um país.\n");
+        printf("Depois da escolha o computador usará a outra carta.\n");
+        printf("O jogador deverá escolher um atributo entre: população, área, pib e densidade populacional.\n");
+        printf("Todos os atributos terão que ser maiores, menos a densidade demográfica\n");
+        printf("Bom jogo!\n");
+        
+
+        break;
+
+        case 4:
+
+        printf("Jogo desenvolvido por Robson Tadeu.\n");
+        printf("Obrigado por jogar.\n");
+
+        break;
+
+        default:
+        printf("Opção inválida, por favor reinicie o jogo.\n");
+
+        return 0;
+
     
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 
 
 
