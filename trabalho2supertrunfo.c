fdda228cedf71160af17656ad2085d302aedc7e5
Robsontmb <rtadeumb@hotmail.com>
2025-03-01 00:17:30 +0000

    trabalho2 super trunfo

--- trabalho2supertrunfo.c
+++ trabalho2supertrunfo.c
@@ -2,14 +2,12 @@
 
 int main () {
 
-    int populacao, populacao2;
-    float area, area2;
-    long long int pib, pib2;
-    int pontosturisticos, pontosturisticos2;
-    double densidadepopulacional, densidadepopulacional2;
-    double pibpercapta, pibpercapta2;
-    double superpoder, superpoder2;
+    int populacao = 211000, populacao2 = 45000;
+    float area= 850.50, area2= 450.30;
+    int pib= 580, pib2= 820;
+    float densidadedemografica = populacao / area , densidadedemografica2= populacao2 / area2;
     int opcao;
+    int pais;
 
     printf("Bem vindo ao jogo Supertrunfo países!\n");
     printf("Escolha uma opção:\n");
@@ -17,33 +15,54 @@ int main () {
     printf("2. como jogar.\n");
     printf("3. Créditos.\n");
     printf("4. Sair.\n");
+    scanf("%d", &opcao);
 
     switch(opcao){
 
         case 1:
 
+        printf("Agora você poderá escolher um país:\n");
+        printf("1.Brasil\n");
+        printf("População: %d\n",populacao);
+        printf("area: %f\n", area);
+        printf("pib: %d\n", pib);
+        printf("densidade demográfica: %.2f\n", densidadedemografica);
+        printf("2. Argentina:\n");
+        printf("População: %d\n",populacao2);
+        printf("area: %f\n", area2);
+        printf("pib: %d\n", pib2);
+        printf("densidade demográfica: %.2f\n", densidadedemografica2);
+
+
+
+
+
+
+
+
+
 
         break;
 
         case 2:
-
-
-        break;
-
-        case 3:
         printf("O jogo é bem simples, o jogador deve escolher uma carta referente a um país.\n");
         printf("Depois da escolha o computador usará a outra carta.\n");
         printf("O jogador deverá escolher um atributo entre: população, área, pib e densidade populacional.\n");
         printf("Todos os atributos terão que ser maiores, menos a densidade demográfica\n");
         printf("Bom jogo!\n");
+
+        break;
+
+        case 3:
+        printf("Jogo desenvolvido por Robson Tadeu.\n");
+        printf("Obrigado por jogar.\n");
         
 
         break;
 
         case 4:
-
-        printf("Jogo desenvolvido por Robson Tadeu.\n");
-        printf("Obrigado por jogar.\n");
+        ("Jogo encerrado!\n");
+        
 
         break;
 
