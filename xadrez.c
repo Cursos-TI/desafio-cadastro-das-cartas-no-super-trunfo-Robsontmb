788eb343971e7c3a319088c0b6bafe464938a052
Robsontmb <rtadeumb@hotmail.com>
2025-03-06 23:01:35 +0000

    trabalho xadrez

--- xadrez.c
+++ xadrez.c
@@ -2,7 +2,7 @@
 
 int main () {
 
-int torre = 0, bispo = 0, rainha;
+int torre = 0, bispo = 0, rainha, cavalo, movimento= 1;
 
 printf("O jogo de xadrez começou!/n");
 printf("A primeira peça a se mover será a torre!\n");
@@ -24,6 +24,21 @@ for(rainha=0;rainha<8;rainha++){
     printf("Rainha foi para esquerda\n");
 
 }
+    for(cavalo=1; cavalo<2; cavalo++) {
+        while(movimento <=2){
+
+            printf("cavalo foi para baixo\n");
+            movimento++;
+        }
+
+        printf("Cavalo foi para esquerda\n");
+
+
+    }
+
+        
+
+
 
     return 0;
 }
